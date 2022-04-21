62b9349a0abeb788c045467c005714b0e25d01ac
maksimselchukou <selchukouwebdev1995@gmail.com>
2022-04-21 15:45:29 +0300

    Project completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ export function Header() {
     return (
         <nav className="deep-purple lighten-3">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React Movies</a>
+                <a href="/" className="brand-logo">React Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#">Repo</a></li>
+                    <li><a href="https://github.com/MaksimSelchukou/react-shop" rel="noreferrer" target={'_blank'}>Repo</a></li>
 
                 </ul>
             </div>
